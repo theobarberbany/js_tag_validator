b2f24f04e5146bb046b4172c0d2acaed3211318a
[Theo Barber-Bany] <theobarberbany@gmail.com>
2017-10-05 17:49:50 +0100

    Update state when button pressed (does not update UI yet..)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
-import {FileHandler} from './components/FileHandler'
+import { createStore } from 'redux';
+import { fileHandler } from './reducers.js';
+import {FileHandler} from './components/FileHandler';
+import CardStatus from './carbon/CardStatus';
 import logo from './logo.svg';
 import './App.css';
+
+import {
+  simulateDrop,
+  copyClipboard,
+  toggleComplete
+} from './actions'
+
+let store = createStore(fileHandler, window.STATE_FROM_SERVER)
+
+// Log the initial state of the store (currently set in reducers.js)
+console.log(store.getState())
+
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+let unsubscribe = store.subscribe(() =>
+console.log(store.getState())
+)
+
+// Dispatch some actions (woo!)
+//store.dispatch(simulateDrop())
 
 class App extends Component {
   simulateFileDrop () {
     //Do some redux state magic here
-    console.log("Eventually this will do something")
+    store.dispatch(simulateDrop())
   }
   render() {
     return (
