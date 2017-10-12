1bb900381d81b9186ab25eedabc0d49291dc63b6
[Theo Barber-Bany] <theobarberbany@gmail.com>
2017-10-12 16:59:21 +0100

    Added some conditional logic to the way handleFileDrop deals with a dropped file

--- src/components/FileHandlerContainer.js
+++ src/components/FileHandlerContainer.js
@@ -4,8 +4,7 @@ import { DragDropContext, DragDropContextProvider } from 'react-dnd'
 import HTML5Backend, { NativeTypes } from 'react-dnd-html5-backend'
 import FileHandler from './FileHandler'
 import * as fileHandlerActions from '../ducks/FileHandlerDuck'
-import FileList from './FileList'
-import {parseData, now} from '../internal/Parser.js'
+import {parseData, now} from '../internal/Parser'
 
 import './FileHandlerContainer.css';
 
@@ -21,37 +20,38 @@ class FileHandlerContainer extends Component {
     
     handleFileDrop(item, monitor) {
         if (monitor) {
-            console.log("you dropped something")
+            console.log("you dropped something..")
             let component = this
-            // On each file drop, reset the state of droppedFiles (a bit hacky.)
-            this.setState({ droppedFiles: [] })
             // Update the state of droppedFiles
             const droppedFiles = monitor.getItem().files
-            // log what type of file was passed
-            console.log(droppedFiles[0].type)
-            // update the state of the ui
-            store.dispatch(fileHandlerActions.dropFile())
-            // update state with dropped file
-            this.setState({ droppedFiles })
-            // log what dropped file is 
-            console.log(this.state.droppedFiles[0])
-            // To Do : set the callback of parseData to push parsed data to store.
-            let parsePromise = new Promise ((resolve, reject) => {
-                let start = now();
-                console.log('Starting parsing')
-                parseData(this.state.droppedFiles[0], function(results) {
-                    component.setState({
-                      parsedData: results.data, //update array with parsed data
-                    })
+            if(droppedFiles[0].type == 'text/csv') {
+                // log what type of file was passed
+                console.log(droppedFiles[0].type)
+                // update the state of the ui
+                store.dispatch(fileHandlerActions.dropFile())
+                // update state with dropped file
+                this.setState({ droppedFiles })
+                // To Do : set the callback of parseData to push parsed data to store.
+                let parsePromise = new Promise ((resolve, reject) => {
+                    let start = now();
+                    console.log('Starting parsing')
+                    parseData(this.state.droppedFiles[0], function(results) {
+                        component.setState({
+                            parsedData: results.data, //update array with parsed data
+                            })
                     let end = now();
                     resolve();
                     console.log('Finished parsing:');
                     console.log("       Time:", (end-start || "(Unknown; your browser does not support the Performance API)"), "ms");
-                })
-            });
-
-            return parsePromise
-            
+                            })
+                    });
+                return parsePromise
+            } else {
+                // On each file drop, reset the state of droppedFiles (a bit hacky.)
+                this.setState({ droppedFiles: [] })
+                // scold the user..
+                console.log("only drop a csv file here...")
+            } 
 
         }
     }
