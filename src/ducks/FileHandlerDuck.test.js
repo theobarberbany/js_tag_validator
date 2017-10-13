dfbfd1423cf4aea20e2a805c4b784adc098cc518
[Theo Barber-Bany] <theobarberbany@gmail.com>
2017-10-13 11:44:01 +0100

    Added test for DROP_FILE action

--- src/ducks/FileHandlerDuck.test.js
+++ src/ducks/FileHandlerDuck.test.js
@@ -4,5 +4,7 @@ test('FileHandler reducers', () => {
     let state;
     state = reducer(undefined, {});
     expect(state).toEqual({cardTitle:'Get started',cardIcon:'copy',cardInfo:['Drop manifest file here'],status:1});
+    state = reducer(undefined, {type:'DROP_FILE'});
+    expect(state).toEqual({cardTitle:'Crunching Numbers',cardIcon:'copy',cardInfo:['Won\'t be a minute'],status:0}); 
   });
   
