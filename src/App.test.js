215dc411f0aca3b981e09a225ec299c1c4090e53
[Theo Barber-Bany] <theobarberbany@gmail.com>
2017-10-13 11:17:21 +0100

    Renaming / comments

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// Ignoring this as it's not really nessecary?
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
--- src/ducks/FileHandlerDuck.test.js
+++ src/ducks/FileHandlerDuck.test.js
@@ -1,6 +1,6 @@
 import reducer from './FileHandlerDuck';
 
-test('reducers', () => {
+test('FileHandler reducers', () => {
     let state;
     state = reducer(undefined, {});
     expect(state).toEqual({cardTitle:'Get started',cardIcon:'copy',cardInfo:['Drop manifest file here'],status:1});
