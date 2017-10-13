7410f6a12b9ec808e6168b3627e52b4aead706b1
[Theo Barber-Bany] <theobarberbany@gmail.com>
2017-10-13 12:05:08 +0100

    Add reducer for PUSH_DATA

--- src/ducks/FileHandlerDuck.js
+++ src/ducks/FileHandlerDuck.js
@@ -4,32 +4,34 @@ import {CardStatus} from 'carbon-components-react';
 //Action types
 export const DROP_FILE = 'DROP_FILE'
 export const TOGGLE_COMPLETE = 'TOGGLE_COMPLETE'
-export const PUSH_OBJECT = 'PUSH_OBJECT'
+export const PUSH_DATA = 'PUSH_DATA'
 
 // Reducer
 
-//Initial state for *this* component (Duck)
+//Initial state for *this* component (Duck) (This only gets passed a slice of the state)
 const initialState = {
-    cardTitle: "Get started",
-    cardIcon: "copy",
-    cardInfo:['Drop manifest file here'], 
-    status: CardStatus.appStatus.NOT_RUNNING,
+        cardTitle: "Get started",
+        cardIcon: "copy",
+        cardInfo:['Drop manifest file here'], 
+        status: CardStatus.appStatus.NOT_RUNNING,
+
 }
 
 export default function reducer(state=initialState, action) {
-    
     switch (action.type) {
-        case 'DROP_FILE':
+        case DROP_FILE:
         return {...state,
             cardTitle: "Crunching Numbers",
             cardIcon: "copy",
             cardInfo:['Won\'t be a minute'], 
             status: CardStatus.appStatus.RUNNING,
         }
-        default: return state
 
-        case 'PUSH_OBJECT':
-        return{}
+        case PUSH_DATA:
+        return {...state,
+            cleanData: action.data
+        }
+        default: return state
     }
     
 }
@@ -37,16 +39,16 @@ export default function reducer(state=initialState, action) {
 // Action Creators
 
 //1. Loads an object into store.
-export const pushObject = (file) => {
+export const pushData = (data) => {
     return {
-        type: 'PUSH_OBJECT',
-        file
+        type: PUSH_DATA,
+        data
     }
 }
 //2. Toggles a critical warning's complete / incomplete feild (notes it as addressed)
 export const toggleComplete = (id) => {
     return {
-        type: 'TOGGLE_COMPLETE',
+        type: TOGGLE_COMPLETE,
         id
     }
 }
@@ -54,7 +56,7 @@ export const toggleComplete = (id) => {
 //3. Changes FileHandler UI state when something is dropped on it
 export const dropFile = (fileHandlerState) => {
     return {
-        type: 'DROP_FILE',
+        type: DROP_FILE,
         fileHandlerState
     }
 }
