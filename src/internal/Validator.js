c2c41dbe138250f9b2431868ba8075e0e3e0e1af
[Theo Barber-Bany] <theobarberbany@gmail.com>
2017-10-17 15:05:43 +0100

    Difference function

--- src/internal/Validator.js
+++ src/internal/Validator.js
@@ -1,10 +1,22 @@
-//@flow
 const testarray = [
   ["TAAGGCGA", "CTCTCTAT"],
   ["CGTACTAG", "CTCTCTAT"],
   ["AGGCAGAA", "CTCTCTAT"]
 ];
 
-export const difference = function(tag1: String, tag2: String) {
+function difference(tag1, tag2) {
   //Check the difference between two tags
-};
+  try {
+    let counter = 0;
+    for (let i = 0; i < tag1.length; i++) {
+      if (tag1[i] != tag2[i]) {
+        counter += 1;
+      }
+    }
+    return counter;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+module.exports = { difference: difference };
--- src/internal/Validator.test.js
+++ src/internal/Validator.test.js
@@ -1,5 +1,5 @@
-const difference = require("./Validator");
+const Validator = require("./Validator");
 
 test('tests the difference between "TAAGGCGA","CGTACTAG" to be 8', () => {
-  expect(difference("TAAGGCGA", "CGTACTAG").toBe(8));
+  expect(Validator.difference("TAAGGCGA", "CGTACTAG")).toBe(8);
 });
