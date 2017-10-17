b723bf7cfe7c17099ab23d583e8a287255d1371a
[Theo Barber-Bany] <theobarberbany@gmail.com>
2017-10-17 16:05:27 +0100

    Error handling

--- src/internal/Validator.js
+++ src/internal/Validator.js
@@ -7,6 +7,9 @@ const testarray = [
 function difference(tag1, tag2) {
   //Check the difference between two tags
   try {
+    if (tag1.length != tag2.length) {
+      throw "Tag Length Mismatch";
+    }
     let counter = 0;
     for (let i = 0; i < tag1.length; i++) {
       if (tag1[i] != tag2[i]) {
@@ -16,6 +19,7 @@ function difference(tag1, tag2) {
     return counter;
   } catch (e) {
     console.log(e);
+    throw Error(e);
   }
 }
 
--- src/internal/Validator.test.js
+++ src/internal/Validator.test.js
@@ -3,3 +3,10 @@ const Validator = require("./Validator");
 test('tests the difference between "TAAGGCGA","CGTACTAG" to be 8', () => {
   expect(Validator.difference("TAAGGCGA", "CGTACTAG")).toBe(8);
 });
+
+test("checks an error is thrown on tag length mismatch", () => {
+  // pass an anonymous function so the function gets executed,
+  //and it's result is thrown to expect() instead of just the
+  //result of the error being thrown
+  expect(() => Validator.difference("TAAGGCGA", "CGTACTA")).toThrowError(Error);
+});
