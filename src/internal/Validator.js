2efc29247ba619776a3fb8b8c46dc5dd68cd5f8e
[Theo Barber-Bany] <theobarberbany@gmail.com>
2017-10-18 12:03:51 +0100

    Check array of tags

--- src/internal/Validator.js
+++ src/internal/Validator.js
@@ -1,11 +1,6 @@
-const testarray = [
-  ["TAAGGCGA", "CTCTCTAT"],
-  ["CGTACTAG", "CTCTCTAT"],
-  ["AGGCAGAA", "CTCTCTAT"]
-];
-
+// See https://github.com/theobarberbany/tag_validator
 function difference(tag1, tag2) {
-  //Check the difference between two tags
+  //compare two tags - return the degree to which they differ
   try {
     if (tag1.length != tag2.length) {
       throw "Tag Length Mismatch";
@@ -23,4 +18,22 @@ function difference(tag1, tag2) {
   }
 }
 
-module.exports = { difference: difference };
+function check_array(array) {
+  // check if an array of tags differ by at least 3.
+  let number_cols = array[0].length;
+  let bad_tags = 0;
+  for (let i = 0; i < number_cols; i++) {
+    for (let j = 0; j < array.length; j++) {
+      for (let k = j + 1; k < array.length; k++) {
+        let diff = difference(array[j][i], array[k][i]);
+        // to do  :  dispatch an action to add this to a 'checklist'
+        diff < 3 ? bad_tags++ : null;
+      }
+    }
+  }
+  return bad_tags;
+}
+module.exports = {
+  difference: difference,
+  check_array: check_array
+};
