0f0697a87a27f9e64fae0cd6b1ba883d92376975
[Theo Barber-Bany] <theobarberbany@gmail.com>
2017-10-18 12:03:29 +0100

    Update tests

--- src/internal/Validator.test.js
+++ src/internal/Validator.test.js
@@ -1,12 +1,27 @@
+const testArray = [
+  ["TAAGGCGA", "CTCTCTAT"],
+  ["CGTACTAG", "CTCTCTAT"],
+  ["AGGCAGAA", "CTCTCTAT"]
+];
+
 const Validator = require("./Validator");
 
-test('tests the difference between "TAAGGCGA","CGTACTAG" to be 8', () => {
-  expect(Validator.difference("TAAGGCGA", "CGTACTAG")).toBe(8);
+describe("Difference function", () => {
+  it('tests the difference between "TAAGGCGA","CGTACTAG" to be 8', () => {
+    expect(Validator.difference("TAAGGCGA", "CGTACTAG")).toBe(8);
+  });
+  it("checks an error is thrown on tag length mismatch", () => {
+    // pass an anonymous function so the function gets executed,
+    //and it's result is thrown to expect() instead of just the
+    //result of the error being thrown
+    expect(() => Validator.difference("TAAGGCGA", "CGTACTA")).toThrowError(
+      Error
+    );
+  });
 });
 
-test("checks an error is thrown on tag length mismatch", () => {
-  // pass an anonymous function so the function gets executed,
-  //and it's result is thrown to expect() instead of just the
-  //result of the error being thrown
-  expect(() => Validator.difference("TAAGGCGA", "CGTACTA")).toThrowError(Error);
+describe("Check an array of tags", () => {
+  it("checks the test array fails", () => {
+    expect(Validator.check_array(testArray)).toBe(3);
+  });
 });
