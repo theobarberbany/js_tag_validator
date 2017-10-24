175afe606cd0712a43253a5ad3bcab9f388b3588
[Theo Barber-Bany] <theobarberbany@gmail.com>
2017-10-24 12:54:35 +0100

    Return accurate information on tags which fail validation (Line number in csv, which tags, difference amount)

--- src/internal/Validator.js
+++ src/internal/Validator.js
@@ -1,5 +1,4 @@
 // See https://github.com/theobarberbany/tag_validator
-
 // Actually taken *from* stack overflow's website...
 //Try "string".formatUnicorn in console when on their website..
 String.prototype.formatUnicorn = function() {
@@ -37,6 +36,7 @@ function call_check_array(array) {
   // If Two Cols : Check against adjacent col
   let number_tag_groups;
   let output;
+  let concatenated;
   //Determine how many tag groups there are
   if (Array.isArray(array[0])) {
     number_tag_groups = array[0].length;
@@ -48,6 +48,7 @@ function call_check_array(array) {
   if (number_tag_groups === 1) {
     console.log("calling check_array_single with data : ", array);
     output = check_array_single(array);
+    return output;
   } else {
     // More than one  - so compare tag1 -> tag2
     console.log(
@@ -59,14 +60,14 @@ function call_check_array(array) {
     );
     output = check_array(array, number_tag_groups);
     //next check the concatenation as a single tag
-    let concatenated = concatenate_tags(array);
+    concatenated = concatenate_tags(array);
     console.log(
       "calling check_array_single with concatenated data : ",
       concatenated
     );
     concatenated = check_array_single(concatenated);
   }
-  return output;
+  return { normal: output, concatenated: concatenated };
 }
 
 function check_array(array, number_tag_groups) {
@@ -76,31 +77,33 @@ function check_array(array, number_tag_groups) {
     "number tag groups: ",
     number_tag_groups
   );
-  let bad_tags = 0;
+  let bad_tag_count = 0;
+  let bad_tag_pairs = [];
   for (let i = 0; i < array.length; i++) {
     let diff = difference(array[i][0], array[i][1]);
     //console.log("Comparing", array[i][0], "to", array[i][1]);
-    // to do  :  dispatch an action to add this to a 'checklist'
-    diff < 3 ? console.log("Above tag bad!") : null;
-    diff < 3 ? bad_tags++ : null;
+    diff < 3
+      ? bad_tag_pairs.push([array[i][0], array[i][1], diff, i + 10])
+      : null;
+    diff < 3 ? bad_tag_count++ : null;
   }
-  return bad_tags;
+  return { bad_tag_count, bad_tag_pairs };
 }
 
 function check_array_single(array) {
   console.log("check_array_single recieved data:", array);
-  let bad_tags = 0;
+  let bad_tag_count = 0;
+  let bad_tag_pairs = [];
   for (let j = 0; j < array.length; j++) {
     for (let k = j + 1; k < array.length; k++) {
       let diff = difference(array[j], array[k]);
       // (diff = difference(array[j][i], array[k][i]));
       //console.log("Comparing", array[j], "to", array[k]);
-      // to do  :  dispatch an action to add this to a 'checklist'
-      diff < 3 ? bad_tags++ : null;
+      diff < 3 ? bad_tag_pairs.push([array[j], array[k]], diff) : null;
+      diff < 3 ? bad_tag_count++ : null;
     }
   }
-  console.log(bad_tags, "bad tags");
-  return bad_tags;
+  return { bad_tag_count, bad_tag_pairs };
 }
 
 function concatenate_tags(array) {
@@ -187,11 +190,13 @@ function check_tag_set_composition(array) {
 function run(array) {
   //Function to tie everything together
   console.log("Checking tags differ by at least 3");
-  let bad_tags = call_check_array(array);
-  console.log("Found ", bad_tags, "bad tags");
+  let bad_tag_container = call_check_array(array);
   console.log("Checking tag set composition");
   let composotion = call_check_tag_set_composition(array);
-  return { bad_tags: bad_tags, composotion: composotion };
+  return {
+    bad_tag_container: bad_tag_container,
+    composotion: composotion
+  };
 }
 
 module.exports = {
