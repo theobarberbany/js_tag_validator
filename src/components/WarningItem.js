f12901fd7594d56dfe2f98285589c670efc5e10b
[Theo Barber-Bany] <theobarberbany@gmail.com>
2017-10-25 18:28:57 +0100

    New container and child item(s) for warnings reesulting from validation output

---
+++ src/components/WarningItem.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import {
+  StructuredListRow,
+  StructuredListInput,
+  StructuredListCell,
+  Icon
+} from "carbon-components-react";
+
+class StructuredListInputNew extends StructuredListInput {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <StructuredListInput />;
+  }
+}
+
+const WarningItem = ({ onChange, tag1, tag2, diff, pos }) => (
+  <StructuredListRow label htmlFor="warning">
+    <StructuredListInput
+      id="warning"
+      value="warning"
+      title="warning"
+      name="warning"
+      onChange={onChange}
+    />
+    <StructuredListCell>
+      <Icon
+        className="bx--structured-list-svg"
+        name="checkmark--glyph"
+        description="select a warning"
+      />
+    </StructuredListCell>
+    <StructuredListCell>{tag1}</StructuredListCell>
+    <StructuredListCell>{tag2}</StructuredListCell>
+    <StructuredListCell>{diff}</StructuredListCell>
+    <StructuredListCell>{pos}</StructuredListCell>
+  </StructuredListRow>
+);
+
+WarningItem.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  tag1: PropTypes.string.isRequired,
+  tag1: PropTypes.string.isRequired,
+  tag2: PropTypes.string.isRequired,
+  diff: PropTypes.number.isRequired,
+  pos: PropTypes.number.isRequired
+};
+
+export default WarningItem;
