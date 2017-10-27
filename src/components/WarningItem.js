2b5146de2be1450f135388dd57c4fbfb7b5db421
[Theo Barber-Bany] <theobarberbany@gmail.com>
2017-10-27 10:53:36 +0100

    Remove unused proptype

--- src/components/WarningItem.js
+++ src/components/WarningItem.js
@@ -32,7 +32,6 @@ const WarningItem = ({ onClick, tag1, tag2, diff, pos }) => (
 WarningItem.propTypes = {
   onClick: PropTypes.func.isRequired,
   tag1: PropTypes.string.isRequired,
-  tag1: PropTypes.string.isRequired,
   tag2: PropTypes.string.isRequired,
   diff: PropTypes.number.isRequired,
   pos: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
