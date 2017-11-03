2ea11bed213b111156277f2ce2bce3c9aeff5ae7
[Theo Barber-Bany] <theobarberbany@gmail.com>
2017-11-03 10:31:31 +0000

    Fix broken tests

--- src/internal/Validator.test.js
+++ src/internal/Validator.test.js
@@ -87,16 +87,16 @@ describe("Checking the composition of each 'column' in a given tag set (oligo)",
   it("Returns an object, containing an array for each column with tallies of each base [A,T,C,G]", () => {
     expect(
       Validator.check_tag_set_composition(["TAAGGCGA", "CGTACTAG", "AGGCAGAA"])
-    ).toEqual({
-      Col0: [1, 1, 1, 0],
-      Col1: [1, 0, 0, 2],
-      Col2: [1, 1, 0, 1],
-      Col3: [1, 0, 1, 1],
-      Col4: [1, 0, 1, 1],
-      Col5: [0, 1, 1, 1],
-      Col6: [2, 0, 0, 1],
-      Col7: [2, 0, 0, 1]
-    });
+    ).toEqual([
+      [1, 1, 1, 0],
+      [1, 0, 0, 2],
+      [1, 1, 0, 1],
+      [1, 0, 1, 1],
+      [1, 0, 1, 1],
+      [0, 1, 1, 1],
+      [2, 0, 0, 1],
+      [2, 0, 0, 1]
+    ]);
   });
 });
 
