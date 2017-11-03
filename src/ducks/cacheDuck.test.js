751cb81e085f77725398a81017607cd4a77741cf
[Theo Barber-Bany] <theobarberbany@gmail.com>
2017-11-03 10:31:13 +0000

    Update tests

--- src/ducks/cacheDuck.test.js
+++ src/ducks/cacheDuck.test.js
@@ -1,4 +1,92 @@
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import fetchMock from "fetch-mock";
+
 import { reducer } from "./cacheDuck";
+import * as duck from "./cacheDuck";
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe("actions", () => {
+  it("should create an action to request a cache fetch", () => {
+    const cacheURL = "http://testurl.fetch/cache.json";
+    const expectedAction = {
+      type: duck.REQUEST_CACHE,
+      cacheURL: cacheURL
+    };
+    expect(duck.requestCache(cacheURL)).toEqual(expectedAction);
+  });
+  it("should create an action to recieve a cache fetch after the async action to carry out the fetch completes", () => {
+    const json = {
+      somejsonobject: "somedata"
+    };
+    const cacheURL = "http://testurl.fetch/cache.json";
+    const expectedAction = {
+      type: duck.RECIEVE_CACHE,
+      cacheURL,
+      data: json,
+      receivedAt: Date.now()
+    };
+    expect(duck.recieveCache(cacheURL, json)).toEqual(expectedAction);
+  });
+});
+
+describe("async actions", () => {
+  afterEach(() => {
+    fetchMock.reset();
+    fetchMock.restore();
+  });
+  beforeEach(() => {
+    // fetchMock.get("*", {
+    //   body: { somejsonobject: "somedata" },
+    //   headers: { "content-type": "application/json" }
+    // });
+    fetchMock.mock("*", {
+      status: 200,
+      body: {
+        somejsonobject: "somedata"
+      },
+      headers: {
+        "content-type": "application/json"
+      }
+    });
+  });
+  it("creates RECIEVE_CACHE when fetching the cache is complete", () => {
+    let p = fetch("/cache").then(res => {
+      return res.json();
+    });
+    p.then(j => {
+      console.log("Res dump: ", j);
+    });
+
+    const expectedActions = [
+      {
+        type: duck.REQUEST_CACHE,
+        cacheURL: "/cache.json"
+      },
+      {
+        type: duck.RECIEVE_CACHE,
+        cacheURL: "/cache.json",
+        data: {
+          body: {
+            somejsonobject: "somedata"
+          }
+        }
+      }
+    ];
+    const store = mockStore({});
+
+    return store.dispatch(duck.fetchCache("/cache.json")).then(() => {
+      //rebodyturn of async actions
+      console.log("store actions: ", store.getActions());
+      let gotActions = store.getActions();
+      let o = gotActions[1];
+      delete o.receivedAt;
+      expect(gotActions).toEqual(expectedActions);
+    });
+  });
+});
 
 test("reducers", () => {
   let state;
@@ -15,7 +103,11 @@ test("reducers", () => {
         badPairs: [],
         badPairsConcat: []
       },
-      cache: { isFetching: false, didInvalidate: false, data: [] }
+      cache: {
+        isFetching: false,
+        didInvalidate: false,
+        data: []
+      }
     },
     {
       type: "REQUEST_CACHE",
@@ -24,17 +116,23 @@ test("reducers", () => {
     }
   );
   expect(state).toEqual({
+    cache: {
+      data: [],
+      didInvalidate: false,
+      isFetching: false
+    },
+    didInvalidate: false,
     fileHandler: {
+      badPairs: [],
+      badPairsConcat: [],
+      cleanData: [],
       displayProps: {
-        cardTitle: "Get started",
         cardIcon: "copy",
         cardInfo: ["Drop manifest file here"],
+        cardTitle: "Get started",
         status: 1
-      },
-      cleanData: [],
-      badPairs: [],
-      badPairsConcat: []
+      }
     },
-    cache: { isFetching: true, didInvalidate: false, data: [] }
+    isFetching: true
   });
 });
