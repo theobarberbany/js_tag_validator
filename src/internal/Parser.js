929ed19e0c58ebde3775c12de7bc056b05eacd0c
[Theo Barber-Bany] <theobarberbany@gmail.com>
2017-11-13 15:29:22 +0000

    Add parseData2

--- src/internal/Parser.js
+++ src/internal/Parser.js
@@ -1,6 +1,7 @@
 import Papa from "papaparse";
 
-let parseData = (path, delimiter, callback) => {
+//todo: Wrap this export in a promise? So the actual promise constructor is not required to be defined at every use
+const parseData = (path, delimiter, callback) => {
   Papa.parse(path, {
     download: true,
     delimiter: delimiter,
@@ -12,10 +13,33 @@ let parseData = (path, delimiter, callback) => {
   });
 };
 
-let now = () => {
+const now = () => {
   return typeof window.performance !== "undefined"
     ? window.performance.now()
     : 0;
 };
 
-export { parseData, now };
+const parseData2 = (path, delimiter) => {
+  console.log("PLEASEWORK");
+  return new Promise((resolve, reject) => {
+    console.log("Inside Promise");
+    Papa.parse(path, {
+      download: true,
+      delimiter: delimiter,
+      newline: "\n",
+      quotes: false,
+      fastMode: true,
+      skipEmptyLines: true,
+      complete: results => {
+        console.log("about to resolve");
+        resolve(results);
+      },
+      error: function(err, file, inputElem, reason) {
+        console.log("error " + file + " " + reason);
+        reject(err);
+      }
+    });
+  });
+};
+
+export { parseData, parseData2, now };
