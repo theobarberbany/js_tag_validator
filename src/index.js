a90775f95f3562c4c52db91b63114fc0fb63cb8b
[Theo Barber-Bany] <theobarberbany@gmail.com>
2018-01-09 15:20:02 +0000

    Remove ServiceWorker.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Raven from "raven-js";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+//import registerServiceWorker from "./registerServiceWorker";
 
 Raven.config("https://509f2c5e443c4252b9eb011cc14bda0f@sentry.io/232560", {
   release: "804aef8675adef2a3eb0c692f8d110e47a6d9cce",
