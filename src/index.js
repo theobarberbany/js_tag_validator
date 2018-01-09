41e146453fa9c3f41aeb9a078860ca5f4d368a4b
[Theo Barber-Bany] <theobarberbany@gmail.com>
2018-01-09 17:00:53 +0000

    Add ReactGA

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Raven from "raven-js";
+import ReactGA from "react-ga";
 import App from "./App";
 //import registerServiceWorker from "./registerServiceWorker";
 
+//Sentry
 Raven.config("https://509f2c5e443c4252b9eb011cc14bda0f@sentry.io/232560", {
   release: "804aef8675adef2a3eb0c692f8d110e47a6d9cce",
   autoBreadcrumbs: true,
   captureUnhandledRejections: true
 }).install();
+
+//Google Analytics
+ReactGA.initialize("UA-112184740-1");
 
 ReactDOM.render(
   <div>
