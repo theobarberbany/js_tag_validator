0f384666dbcfb9178ee0aba8a803221c315008f0
Theo Barber-Bany <theobarberbany@gmail.com>
2018-08-24 13:42:09 +0100

    Remove sentry

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import App from "./App";
 //import registerServiceWorker from "./registerServiceWorker";
 
 //Sentry
-Raven.config("https://509f2c5e443c4252b9eb011cc14bda0f@sentry.io/232560", {
-  release: "804aef8675adef2a3eb0c692f8d110e47a6d9cce",
-  autoBreadcrumbs: true,
-  captureUnhandledRejections: true
-}).install();
+//Raven.config("https://509f2c5e443c4252b9eb011cc14bda0f@sentry.io/232560", {
+//  release: "804aef8675adef2a3eb0c692f8d110e47a6d9cce",
+//  autoBreadcrumbs: true,
+//  captureUnhandledRejections: true
+//}).install();
 
 ReactDOM.render(
   <div>
